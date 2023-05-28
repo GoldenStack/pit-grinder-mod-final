19dc0d1f97d0ef5285c87d181cda1ff8b6109ac1
GoldenStack <goldenfire64yt@gmail.com>
2023-05-28 01:57:52 -0500

    Add dedicated API file with temporary solution for API keys

--- src/main/java/boats/jojo/grindbot/GrindBot.java
+++ src/main/java/boats/jojo/grindbot/GrindBot.java
@@ -5,14 +5,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.zip.DataFormatException;
 import java.util.zip.Deflater;
 import java.util.zip.Inflater;
 import java.io.ByteArrayOutputStream;
 
-import net.minecraft.block.Block;
-import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import org.apache.commons.io.IOUtils;
@@ -31,13 +28,7 @@ import java.util.concurrent.ForkJoinPool;
 import java.io.IOException;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.item.EntityItem;
-import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.AxisAlignedBB;
-import net.minecraft.util.BlockPos;
 import net.minecraft.util.StringUtils;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
@@ -45,8 +36,6 @@ import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.Loader;
-import net.minecraftforge.fml.common.ModContainer;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
@@ -62,11 +51,6 @@ public class GrindBot {
 
 	public static final Base64.Encoder BASE64_ENCODER = Base64.getEncoder();
 	public static final Base64.Decoder BASE64_DECODER = Base64.getDecoder();
-
-	public static final String API_URL = "https://pit-grinder-logic-api-jlrw3.ondigitalocean.app/api/grinder";
-	//public static final String API_URL = "http://127.0.0.1:5000/api/grinder"; // testing url
-
-	static String apiKey;
 	
 	Minecraft mcInstance = Minecraft.getMinecraft();
 
@@ -361,7 +345,7 @@ public class GrindBot {
 		try {
 			System.out.println("set key");
 
-			apiKey = String.join("\n", Files.readAllLines(file));
+			GrinderAPI.setApiKey(String.join("\n", Files.readAllLines(file)));
 		} catch (IOException exception) {
 			System.out.println("reading key error");
 			exception.printStackTrace();
@@ -372,203 +356,25 @@ public class GrindBot {
 	
 	public void callBotApi() {
 		// set key from file if unset
-		if (apiKey == null) {
+		if (!GrinderAPI.hasApiKey()) {
 			reloadKey();
 		}
 
 		// return if key is still null - no key was read so no point calling API
-		if (apiKey == null) {
+		if (!GrinderAPI.hasApiKey()) {
 			return;
 		}
 
-		EntityPlayerSP player = mcInstance.thePlayer;
-		
-		makeLog("getting api url: " + API_URL);
+		makeLog("getting api url: " + GrinderAPI.API_URL);
 		
 		preApiProcessingTime = System.currentTimeMillis();
 		
 		// construct client info string
+		String builtInfo = GrinderAPI.buildPayload(this);
 
-		String dataSeparator = "##!##";
-		StringJoiner info = new StringJoiner(dataSeparator);
-
-		info.add(apiKey); // Auth key
-		info.add(player.getName()); // Client username
-		info.add(player.getGameProfile().getId().toString()); // Client UUID - will be null when in offline mode, but Hypixel is online
-
-		// Client position and rotation
-		info.add(join(":::", player.posX, player.posY, player.posZ, player.rotationPitch, player.rotationYaw));
-
-		{ // Client inventory
-			StringJoiner invStr = new StringJoiner("!!!");
-
-			for (ItemStack item : player.inventoryContainer.getInventory()) {
-				if (item == null) {
-					invStr.add(join(":::", "air", 0));
-				} else {
-					invStr.add(join(":::", item.getItem().delegate.getResourceName().getResourcePath(), item.stackSize));
-				}
-			}
-
-			info.add(invStr.toString());
-		}
-
-		{ // Players
-			List<EntityPlayer> playerList = mcInstance.theWorld.playerEntities.stream()
-					.filter(user -> !user.isInvisible())
-					.limit(128)
-					.collect(Collectors.toList());
-
-			StringJoiner playersStr = new StringJoiner("!!!");
-
-			for (EntityPlayer entityPlayer : playerList) {
-				BlockPos block = entityPlayer.getPosition();
-
-				playersStr.add(join(":::", entityPlayer.getName(), block.getX(), block.getY(), block.getZ(), entityPlayer.getHealth(), entityPlayer.getTotalArmorValue()));
-			}
-
-			info.add(playersStr.toString());
-		}
-
-		{ // Middle block
-			Block middleBlock = mcInstance.theWorld.getBlockState(new BlockPos(0, (int) mcInstance.thePlayer.posY - 1, 0)).getBlock();
-			String blockName = middleBlock.delegate.getResourceName().getResourcePath();
-
-			info.add(blockName);
-		}
-
-		{ // Last chat message
-			String chatMsgSeparator = "!#!";
-
-			StringJoiner chatMessages = new StringJoiner(chatMsgSeparator);
-
-			for (int i = 0; i < Math.min(32, chatMsgs.size()); i++) {
-				chatMessages.add(chatMsgs.get(i).replaceAll(dataSeparator, "").replaceAll(chatMsgSeparator, ""));
-			}
-
-			info.add(chatMessages.toString());
-
-			chatMsgs.clear();
-		}
-
-		{ // Container items
-			List<ItemStack> containerItems = mcInstance.thePlayer.openContainer.getInventory();
-
-			if (containerItems.size() <= 46) { // exit if a container is open (definitely a better way to do that)
-				info.add("null");
-			} else {
-				StringJoiner items = new StringJoiner("!!!");
-
-				for (int i = 0; i < containerItems.size() - 36; i++) { // minus 36 to cut off inventory
-					ItemStack item = containerItems.get(i);
-
-					if (item == null) {
-						items.add(join(":::", "air", "air", 0));
-					} else {
-						items.add(join(":::", item.getItem().delegate.getResourceName().getResourcePath(), item.stackSize, item.getDisplayName()));
-					}
-				}
-
-				info.add(items.toString());
-			}
-		}
-
-		{ // Dropped items
-			StringJoiner droppedItemsStr = new StringJoiner("!!!");
-
-			AxisAlignedBB box = new AxisAlignedBB(player.getPosition(), player.getPosition()).expand(32, 32, 32);
-			List<EntityItem> droppedItems = mcInstance.theWorld.getEntitiesWithinAABB(EntityItem.class, box);
-
-			for (int i = 0; i < Math.min(128, droppedItems.size()); i++) {
-				EntityItem curItem = droppedItems.get(i);
-
-				String curItemName = curItem.getEntityItem().getItem().delegate.getResourceName().getResourcePath();
-
-				BlockPos itemPos = curItem.getPosition();
-
-				droppedItemsStr.add(join(":::", curItemName, itemPos.getX(), itemPos.getY(), itemPos.getZ()));
-			}
-
-			info.add(droppedItemsStr.toString());
-		}
-		
-		// Important chat message
-		info.add(importantChatMsg.equals("") ? "null" : importantChatMsg);
+		// Cleanup data that will be sent
+		chatMsgs.clear();
 		importantChatMsg = "";
-		
-		// Current open GUI
-		info.add(mcInstance.currentScreen == null ? "null" : mcInstance.currentScreen.getClass().toString());
-
-		{ // Villager positions
-			StringJoiner villagersStr = new StringJoiner("!!!");
-
-			List<Entity> villagerEntities = mcInstance.theWorld.getLoadedEntityList()
-					.stream().filter(entity -> entity.getClass().equals(EntityVillager.class)).collect(Collectors.toList());
-
-			for (int i = 0; i < Math.min(8, villagerEntities.size()); i++) {
-				BlockPos villagerPos = villagerEntities.get(i).getPosition();
-
-				villagersStr.add(join(":::", villagerPos.getX(), villagerPos.getY(), villagerPos.getZ()));
-			}
-
-			info.add(villagersStr.toString());
-		}
-
-		info.add(String.valueOf(player.getHealth())); // Client health
-
-		info.add(String.valueOf(player.experienceLevel)); // Client XP level
-
-		// Mod version
-		ModContainer modContainer = Loader.instance().getIndexedModList().get("keystrokesmod");
-		info.add(modContainer == null ? null : modContainer.getVersion());
-
-		{ // Blocks proximity player
-			StringBuilder proximityBlocksStr = new StringBuilder();
-
-			int proximityBlocksHorizontalRange = 8;
-			int proximityBlocksVerticalRange = 2;
-			int playerPosX = (int) player.posX;
-			int playerPosY = (int) player.posY;
-			int playerPosZ = (int) player.posZ;
-
-			for (int x = playerPosX - proximityBlocksHorizontalRange; x <= playerPosX + proximityBlocksHorizontalRange; x++) {
-				for (int y = playerPosY - proximityBlocksVerticalRange; y <= playerPosY + proximityBlocksVerticalRange; y++) {
-					for (int z = playerPosZ - proximityBlocksHorizontalRange; z <= playerPosZ + proximityBlocksHorizontalRange; z++) {
-
-						Map<String, Character> blockChars = new HashMap<>();
-						blockChars.put("minecraft:air", '0');
-						blockChars.put("minecraft:obsidian", 'o');
-						blockChars.put("minecraft:glass", 'g');
-						blockChars.put("minecraft:slime", 's');
-						blockChars.put("minecraft:enchanting_table", 'e');
-						blockChars.put("minecraft:ender_chest", 'c');
-						blockChars.put("minecraft:sea_lantern", 'l');
-						blockChars.put("minecraft:carpet", 'r');
-						blockChars.put("char '.' is reserved for other blocks or error, do not use", '.');
-
-						char blockChar;
-
-						try { // i am scared of it not working with some blocks
-							String blockName = mcInstance.theWorld.getBlockState(new BlockPos(x, y, z)).getBlock().getRegistryName();
-							blockChar = blockChars.getOrDefault(blockName, '.');
-						} catch (Exception e) {
-							e.printStackTrace();
-							proximityBlocksStr.append('.');
-							continue;
-						}
-
-						proximityBlocksStr.append(blockChar);
-					}
-				}
-			}
-
-			info.add(proximityBlocksStr.toString());
-		}
-
-		info.add(String.valueOf(apiLastPing)); // Ping
-
-		// Build string and replace newlines because they apparently mess with the header
-		String builtInfo = info.toString().replaceAll("\n", " ");
 
 		// Compress and add "this is compressed" tag to support API version compatibility
 		String finalBuiltInfo = compressString(builtInfo) + "xyzcompressed";
@@ -583,7 +389,7 @@ public class GrindBot {
 		long preApiGotTime = System.currentTimeMillis();
 
 		ForkJoinPool.commonPool().execute(() -> {
-			HttpGet get = new HttpGet(API_URL);
+			HttpGet get = new HttpGet(GrinderAPI.API_URL);
 
 			int timeoutMs = 5000;
 			RequestConfig requestConfig = RequestConfig.custom()
@@ -999,14 +805,6 @@ public class GrindBot {
 
 	public void setWindowTitle() {
 		Display.setTitle("Jojo Grinder - " + mcInstance.thePlayer.getName());
-	}
-
-	public static String join(@NotNull CharSequence delimiter, @NotNull Object @NotNull ... objects) {
-		return String.join(delimiter, Arrays.stream(objects).map(Object::toString).toArray(CharSequence[]::new));
-	}
-
-	public static String join(@NotNull CharSequence delimiter, @NotNull Collection<@NotNull Object> objects) {
-		return String.join(delimiter, objects.stream().map(Object::toString).toArray(CharSequence[]::new));
 	}
 
 }
